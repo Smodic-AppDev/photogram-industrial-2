5c0e28e530038b16f40df17cd7f37cc9a4036ae0
dsmodic <dsmodic@chicagobooth.edu>
2021-04-21 00:34:19 +0000

    Added indirect assocations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -23,6 +23,9 @@ class Photo < ApplicationRecord
   belongs_to :owner, class_name: "User", counter_cache: true
 
   has_many :comments
+
   has_many :likes
-  
+
+  has_many :fans, through: :likes, source: :fan #source not needed since fan is conventional
+
 end
